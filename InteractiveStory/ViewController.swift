274438435d96655bce9ffda028142d4e4a1dad2e
webergd <kabar3@gmail.com>
2016-06-23 20:40:36 -0700

    Added the ability to type your own name

--- InteractiveStory/ViewController.swift
+++ InteractiveStory/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum Error: ErrorType {
+        case NoName
+    }
+    
+    @IBOutlet weak var nameTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +30,24 @@ class ViewController: UIViewController {
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "startAdventure" { //that way this shit only happens when a specific seque is activated
-            if let PageController = segue.destinationViewController as? PageController {
-                PageController.page = Adventure.story
+            
+            
+            
+            do {
+                if let name = nameTextField.text {
+                    //this returns an optional but actually always has a value of an empty string or text. So we need an additional check:
+                    if name == "" {
+                        throw Error.NoName //this must be handled within the method because prepareForSegue is a non-throwing method and we can't change that.
+                    }
+                    
+                    if let PageController = segue.destinationViewController as? PageController {
+                        PageController.page = Adventure.story(name)
+                    }
+                }
+            } catch {
+                //this will be tackled in next video
             }
+            
         }
     }
 
